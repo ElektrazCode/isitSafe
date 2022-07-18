b1fe25c97e4a676abd1a0ecfd33598608c1e1bb4
ElektrazCode <ElektrazCode@gmail.com>
2022-07-18 18:56:33 -0400

    Added delete and update features

--- public/main.js
+++ public/main.js
@@ -1,29 +1,87 @@
 const updateAllergyBtns = document.querySelectorAll('.editAllergyBtn');
 const removeAllergyBtns = document.querySelectorAll('.deleteAllergyBtn');
 
+const updateSafeBtns = document.querySelectorAll('.editSafeBtn');
+const removeSafeBtns = document.querySelectorAll('.deleteSafeBtn');
+
 updateAllergyBtns.forEach(btn => btn.addEventListener('click', updateAllergy));
-// removeAllergyBtns.forEach(btn => btn.addEventListener('click', removeAllergy(e)));
+removeAllergyBtns.forEach(btn => btn.addEventListener('click', removeAllergy));
+
+updateSafeBtns.forEach(btn => btn.addEventListener('click', updateSafe));
+removeSafeBtns.forEach(btn => btn.addEventListener('click', removeSafe));
 
 function updateAllergy(){
     const item = this.parentElement.parentElement.querySelector('span').textContent;
     const value = document.querySelector('.allergyValue').value;
-    console.log(value);
     fetch('/allergies', {
         method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            allergy: item,
+            value: value
+        })
+    })
+    .then(res => {
+        if(res.ok)
+            return res.json();
+    })
+    .then(date=>{
+        window.location.reload();
+    })
+}
+
+function removeAllergy(){
+    const item = this.parentElement.parentElement.querySelector('span').textContent;
+    fetch('/allergies', {
+        method: 'delete',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
             allergy: item
         })
     })
+    .then(res => {
+        if(res.ok)
+            return res.json();
+    })
+    .then(data=>{
+        window.location.reload();
+    })
 }
 
-// function removeAllergy(e){
-//     console.log(e.target);
-//     fetch('/allergies', {
-//         method: 'delete',
-//         headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify({
-//             allergy: e.target
-//         })
-//     })
-// }
+function updateSafe(){
+    const item = this.parentElement.parentElement.querySelector('span').textContent;
+    const value = document.querySelector('.safeValue').value;
+    fetch('/safeFood', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            safe: item,
+            value: value
+        })
+    })
+    .then(res => {
+        if(res.ok)
+            return res.json();
+    })
+    .then(date=>{
+        window.location.reload();
+    })
+}
+
+function removeSafe(){
+    const item = this.parentElement.parentElement.querySelector('span').textContent;
+    fetch('/safeFood', {
+        method: 'delete',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            safe: item
+        })
+    })
+    .then(res => {
+        if(res.ok)
+            return res.json();
+    })
+    .then(data=>{
+        window.location.reload();
+    })
+}
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const connectionString ='mongodb+srv://moi:tusaisquoi@cluster0.nphvezo.mongodb.net/?retryWrites=true&w=majority'
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 
 MongoClient.connect(connectionString, {useUnifiedtopology: true})
     .then(client => {
@@ -15,6 +15,7 @@ MongoClient.connect(connectionString, {useUnifiedtopology: true})
         app.set('view engine', 'ejs');
 
         app.use(bodyParser.urlencoded({extended:true}));
+        app.use(bodyParser.json());
         app.use(express.static('public'));
 
         app.get('/', (req, res) => {
@@ -22,17 +23,17 @@ MongoClient.connect(connectionString, {useUnifiedtopology: true})
             db.collection('allergies').find().toArray()
                 .then(results => {
                     results1 = results;
+                    db.collection('safeFood').find().toArray()
+                    .then(results => {
+                        results2 = results;
+                        res.render('index.ejs', {allergies: results1, safeFood: results2});   
+                    })        
                 })
-            db.collection('safeFood').find().toArray()
-                .then(results => {
-                    results2 = results;
-                    res.render('index.ejs', {allergies: results1, safeFood: results2});   
-                })          
                 .catch(error => console.error(error))     
         });
         
         app.post('/allergies', (req, res) => {
-            console.log(req.body);
+
             allergiesCollection.insertOne(req.body)
                 .then(result => {
                     res.redirect('/');
@@ -40,7 +41,7 @@ MongoClient.connect(connectionString, {useUnifiedtopology: true})
                 .catch(error => console.error(error))
         });
         app.post('/safeFood', (req, res) => {
-            console.log(req.body);
+
             safeFoodCollection.insertOne(req.body)
                 .then(result => {
                     res.redirect('/');
@@ -64,7 +65,47 @@ MongoClient.connect(connectionString, {useUnifiedtopology: true})
                 })
                 .catch(error => console.error(error))
         });
-        app.listen(3000,() => console.log('listening to 3000'));
+        app.put('/safeFood', (req, res) => {
+            safeFoodCollection.findOneAndUpdate(
+                {
+                    safe: req.body.safe
+                },
+                {
+                    $set: {
+                        safe: req.body.value
+                    }
+                }
+            )
+                .then(result => {
+                    res.redirect('/');
+                })
+                .catch(error => console.error(error))
+        });
+        app.delete('/allergies', (req, res)=>{
+            allergiesCollection.findOneAndDelete(
+                {allergy: req.body.allergy} 
+            )
+            .then(result=> {
+                if(result.deleteCount === 0)
+                    return res.json('No allergies to delete');
+                res.json('Deleted allergy');
+            })
+            .catch(error=>console.error(error))
+        })
+
+        app.delete('/safeFood', (req, res)=>{
+            safeFoodCollection.findOneAndDelete(
+                {safe: req.body.safe} 
+            )
+            .then(result=> {
+                if(result.deleteCount === 0)
+                    return res.json('No safe food to delete');
+                res.json('Deleted safe food');
+            })
+            .catch(error=>console.error(error))
+        })
+
+        app.listen(PORT,() => console.log('listening to 3000'));
 
     })
     .catch(error => console.error(error));
